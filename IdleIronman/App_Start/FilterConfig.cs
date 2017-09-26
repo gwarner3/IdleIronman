76c4ac32f27b3048f15e24ad2e90392d2407f4ce
gwarner3 <george@georgedevelops.com>
2017-09-26 14:19:33 -0500

    Initial Commit

---
+++ IdleIronman/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IdleIronman
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
