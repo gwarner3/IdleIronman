2f96090a08f4683dab905cb8a3e2cabc006b4457
gwarner3 <george@georgedevelops.com>
2017-09-27 10:32:59 -0500

    Created IronManRuleModels

---
+++ IdleIronman/Models/IronManRuleModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdleIronman.Models
+{
+    public class IronManRuleModels
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Start")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Swim Distance")]
+        public double SwimDistanceInMiles { get; set; }
+
+        [Display(Name = "Bike Distance")]
+        public double BikeDistancenIMiles { get; set; }
+
+        [Display(Name = "Run Distance")]
+        public double RunDistanceInMiles { get; set; }
+    }
+}
