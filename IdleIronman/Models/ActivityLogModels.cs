97b276ac0b761d94a47a66b9ac59f555e64502c9
gwarner3 <george@georgedevelops.com>
2017-09-27 14:43:14 -0500

    Added ActivityLogModels

---
+++ IdleIronman/Models/ActivityLogModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdleIronman.Models
+{
+    public class ActivityLogModels
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Log Date")]
+        [Required]
+        public DateTime ActivityDate { get; set; }
+
+        public double? Distance { get; set; }
+
+        [Display(Name = "Duration")]
+        public int? DurationInMinutes { get; set; }
+
+        public ExerciseTypeModels ExerciseTypeModels { get; set; }
+
+        public int ExerciseTypeModelsId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+    }
+}
