d2d768aa11c059675b5d9e7debc13fa996013bed
gwarner3 <george@georgedevelops.com>
2017-09-27 09:41:02 -0500

    Added ExerciseModel

---
+++ IdleIronman/Models/ExerciseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdleIronman.Models
+{
+    public class ExerciseModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Exercise")]
+        public string Name { get; set; }
+    }
+}
