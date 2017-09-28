fc9579f20c9ce15526f0cc3a36ca35ed82c633b1
gwarner3 <george@georgedevelops.com>
2017-09-28 15:07:44 -0500

    Added ParticipantStatsViewModel to use in index view of participant

---
+++ IdleIronman/ViewModels/ParticpantStatsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdleIronman.Models;
+
+namespace IdleIronman.ViewModels
+{
+    public class ParticpantStatsViewModel
+    {
+        public ActivityLogModels ActivityLog { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public IronManRuleModels IronManRules { get; set; }
+
+        public double ParticipantTotalSwimDistance { get; set; }
+
+        public double ParticipantTotalRunDistance { get; set; }
+
+        public double ParticipantTotalBikedDistance { get; set; }
+
+        public DateTime DaysUntilEnd { get; set; }
+    }
+}
