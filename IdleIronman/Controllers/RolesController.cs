1deb390b3f12739bbc3fc1322c0f81cbbbb5b806
gwarner3 <george@georgedevelops.com>
2017-09-28 08:43:28 -0500

    Added participant, teamcaptain, and roles controllers. Roles controller accounts for participant and team captain

---
+++ IdleIronman/Controllers/RolesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdleIronman.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: Roles
+        public ActionResult Index()
+        {
+
+            if (User.IsInRole("CanManagePersonalData"))
+            {
+                return RedirectToAction("Index", "Participant");
+            }
+
+            if (User.IsInRole("CanManageTeamData"))
+            {
+                return RedirectToAction("Index", "TeamCaptain");
+            }
+
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ IdleIronman/Controllers/TeamCaptainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdleIronman.Controllers
+{
+    public class TeamCaptainController : Controller
+    {
+        // GET: TeamCaptain
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
