77b99a47e7494c5cb11131a644b475b715968270
gwarner3 <george@georgedevelops.com>
2017-09-28 11:29:48 -0500

    Added ActivityLogViewModel

---
+++ IdleIronman/ViewModels/ActivityLogViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using IdleIronman.Models;
+
+namespace IdleIronman.ViewModels
+{
+    public class ActivityLogViewModel
+    {
+        public ActivityLogModels ActivityLog { get; set; }
+
+        [Display(Name = "Exercise Completed")]
+        public IEnumerable<ExerciseTypeModels> ExerciseType { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
