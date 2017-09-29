70408e1e53a07f4aca77ea37d7e49b1f8c23fc29
gwarner3 <george@georgedevelops.com>
2017-09-29 14:03:38 -0500

    Added psuedo code that may be used to validate date input of activity

--- IdleIronman/ViewModels/ActivityLogViewModel.cs
+++ IdleIronman/ViewModels/ActivityLogViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using IdleIronman.Helpers;
 using IdleIronman.Models;
 
 namespace IdleIronman.ViewModels
@@ -10,6 +11,9 @@ namespace IdleIronman.ViewModels
     public class ActivityLogViewModel
     {
         public ActivityLogModels ActivityLog { get; set; }
+
+        //may use variable below to validate date entry
+        //public DateTime activityDate { get; set; }
 
         [Display(Name = "Exercise Completed")]
         public IEnumerable<ExerciseTypeModels> ExerciseType { get; set; }
