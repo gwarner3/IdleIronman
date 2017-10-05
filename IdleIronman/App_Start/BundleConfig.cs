d46fddb589f46a32532ba7850118bbef2e818bfd
gwarner3 <george@georgedevelops.com>
2017-10-05 15:26:45 -0500

    Added jquery datatable functionality

--- IdleIronman/App_Start/BundleConfig.cs
+++ IdleIronman/App_Start/BundleConfig.cs
@@ -9,7 +9,10 @@ namespace IdleIronman
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/datatables/jquery.datatables.js",
+                        "~/Scripts/datatables/datatables.bootstrap.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
