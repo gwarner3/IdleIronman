61f34978ef4c6c782aa9d290c9d43a6d7c5d284d
gwarner3 <george@georgedevelops.com>
2018-12-25 22:19:30 -0600

    Activies api added

---
+++ IdleIronman/Controllers/API/ActivitiesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IdleIronman.Models;
+
+namespace IdleIronman.Controllers.API
+{
+    public class ActivitiesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public ActivitiesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET api/activities
+        public IEnumerable<ActivityLogModels> GetActivityLogs()
+        {
+           return  _context.ActivityLogs.ToList();
+        }
+
+        // POST an activity /api/activities/
+        [HttpPost]
+        public ActivityLogModels LogActivity(ActivityLogModels activity)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.ActivityLogs.Add(activity);
+            _context.SaveChanges();
+
+            return activity;
+        }
+
+        //PUT /api/activities/1
+        [HttpPut]
+        public void EditActivity(int id, ActivityLogModels activity)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var activityInDb = _context.ActivityLogs.SingleOrDefault(a => a.Id == id);
+
+            if (activityInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            activityInDb.ActivityDate = activity.ActivityDate;
+            activityInDb.Distance = activity.Distance;
+            activityInDb.DurationInMinutes = activity.DurationInMinutes;
+            activityInDb.ExerciseTypeModelsId = activity.ExerciseTypeModelsId;
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/activities/1
+        [HttpDelete]
+        public void DeleteActivity(int id)
+        {
+            var activityInDb = _context.ActivityLogs.SingleOrDefault(a => a.Id == id);
+
+            if (activityInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.ActivityLogs.Remove(activityInDb);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
--- IdleIronman/Controllers/API/ParticipantsController.cs
+++ IdleIronman/Controllers/API/ParticipantsController.cs
@@ -18,9 +18,15 @@ namespace IdleIronman.Controllers.API
             _context = new ApplicationDbContext();
         }
         // GET /api/participants
-        public IEnumerable<ExerciseTypeModels> GetExerciseTypeModels()
+        //public IEnumerable<ExerciseTypeModels> GetExerciseTypeModels()
+        //{
+        //    return _context.ExerciseTypes.ToList();
+        //}
+
+        // GET /api/participants
+        public IEnumerable<ApplicationUser> GetParticipants()
         {
-            return _context.ExerciseTypes.ToList();
+            return _context.Users.ToList();
         }
 
         //GET a users activities /api/participants/stringID
@@ -40,61 +46,6 @@ namespace IdleIronman.Controllers.API
             }
 
             return exerciseTpy;
-        }
-
-        // POST an activity /api/participants
-        [HttpPost]
-        public ActivityLogModels LoagActivity(ActivityLogModels activity)
-        {
-            if (!ModelState.IsValid)
-            {
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
-            }
-
-            _context.ActivityLogs.Add(activity);
-            _context.SaveChanges();
-
-            return activity;
-        }
-
-        //PUT /api/participants/1
-
-        [HttpPut]
-        public void EditActivity(int id, ActivityLogModels activity)
-        {
-            if (!ModelState.IsValid)
-            {
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
-            }
-
-            var activityInDb = _context.ActivityLogs.SingleOrDefault(a => a.Id == id);
-
-            if (activityInDb == null)
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-
-            activityInDb.ActivityDate = activity.ActivityDate;
-            activityInDb.Distance = activity.Distance;
-            activityInDb.DurationInMinutes = activity.DurationInMinutes;
-            activityInDb.ExerciseTypeModelsId = activity.ExerciseTypeModelsId;
-
-            _context.SaveChanges();
-        }
-
-        // DELETE /api/participant/1
-        [HttpDelete]
-        public void DeleteActivity(int id)
-        {
-            var activityInDb = _context.ActivityLogs.SingleOrDefault(a => a.Id == id);
-
-            if (activityInDb == null)
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-
-            _context.ActivityLogs.Remove(activityInDb);
-            _context.SaveChanges();
         }
     }
 }
